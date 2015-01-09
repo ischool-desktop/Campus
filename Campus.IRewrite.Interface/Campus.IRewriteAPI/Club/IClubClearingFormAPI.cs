6212b5b97803a9ecb4dffba4eca2eacd2bba520c
dylan.huang <dylan.huang@ischool.com.tw>
2015-01-09 15:32:12 +0800

    新增一個介面專用模組_0109

---
+++ Campus.IRewrite.Interface/Campus.IRewriteAPI/Club/IClubClearingFormAPI.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Campus.IRewrite.Interface
+{
+    /// <summary>
+    /// 社團系統 - 學期結算
+    /// </summary>
+    public interface IClubClearingFormAPI
+    {
+        //社團系統 - 學期結算
+        FISCA.Presentation.Controls.BaseForm CreateBasicForm();
+    }
+}
---
+++ Campus.IRewrite.Interface/Campus.IRewriteAPI/Club/IClubDetailItemAPI.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Campus.IRewrite.Interface
+{
+    /// <summary>
+    /// 社團系統 - 社團基本資料
+    /// </summary>
+    public interface IClubDetailItemAPI
+    {
+        //社團基本資料項目
+        FISCA.Presentation.IDetailBulider CreateBasicInfo();
+    }
+}
